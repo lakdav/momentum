cd7f1901c49b87c2223eccf3b182033751723ea8
lakdav <ladav2018@gmail.com>
2023-02-20 13:03:59 +0300

    update:class Time

--- src/app/Time.ts
+++ src/app/Time.ts
@@ -1,17 +1,22 @@
 export default class Time {
-	#name: string = '';
+	#name: string | null = '';
 	#timeContainer: HTMLDivElement;
 	#dayContainer: HTMLDivElement;
-	#greetingContainer: HTMLInputElement;
+	#greetingContainer: HTMLDivElement;
+	#input: HTMLInputElement;
 	#options: Intl.DateTimeFormatOptions = {
 		month: 'long',
 		weekday: 'long',
 		day: 'numeric',
 	};
-	constructor(container: HTMLDivElement, dayContainer: HTMLDivElement, greetingContainer: HTMLInputElement) {
+	constructor(container: HTMLDivElement, dayContainer: HTMLDivElement, greetingContainer: HTMLDivElement) {
 		this.#timeContainer = container;
 		this.#dayContainer = dayContainer;
 		this.#greetingContainer = greetingContainer;
+		this.#input = this.#greetingContainer.querySelector('input') as HTMLInputElement;
+		this.#getLocalStorage();
+		this.#setLocalStorage();
+		this.#setName();
 	}
 	#time() {
 		const date = new Date();
@@ -19,7 +24,47 @@ export default class Time {
 		this.#dayContainer.textContent = date.toLocaleDateString('ru-RU', this.#options);
 	}
 	#greeting() {
-		const text = this.#name;
+		const hr = new Date().getHours();
+		let datePart = this.getDayPart(hr);
+
+		this.#greetingContainer.firstElementChild!.textContent = `Good ${datePart}`;
+		if (this.#name) {
+			this.#input.value = this.#name
+				.split('')
+				.map((char, i) => {
+					return i === 0 ? char.toUpperCase() : char;
+				})
+				.join('');
+		}
+	}
+	#setLocalStorage() {
+		document.addEventListener('visibilitychange', () => {
+			if (document.visibilityState === 'hidden' && this.#name) {
+				localStorage.setItem('name', this.#name);
+			}
+		});
+	}
+	#setName() {
+		this.#input.addEventListener('input', () => {
+			this.#name = this.#input.value;
+		});
+	}
+	#getLocalStorage() {
+		this.#name = localStorage.getItem('name') ? localStorage.getItem('name') : this.#name;
+		this.#greeting();
+	}
+	getDayPart(hour: number) {
+		let partOfDay: string = '';
+		if (hour < 12) {
+			partOfDay = 'morning';
+		}
+		if (12 <= hour && hour < 16) {
+			partOfDay = 'afternoon';
+		}
+		if (16 <= hour && hour < 24) {
+			partOfDay = 'evening';
+		}
+		return partOfDay;
 	}
 	init() {
 		this.#time();
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import Time from './Time';
 const app = () => {
 	const timeContainer = document.querySelector('.time') as HTMLDivElement;
 	const dayContainer = document.querySelector('.date') as HTMLDivElement;
-	const greetingContainer = document.querySelector('.greeting-container > input') as HTMLInputElement;
+	const greetingContainer = document.querySelector('.greeting-container') as HTMLDivElement;
 	const time = new Time(timeContainer, dayContainer, greetingContainer);
 	const showTime = () => {
 		time.init();
